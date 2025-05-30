ac5e0b52968f0078c58969a47bcbd7f04eda733e
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-05-30 12:48:22 +0530

    changes for mouse over

--- First Project/script.js
+++ First Project/script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function mouseFollower() {
+    window.addEventListener('mousemove', function (det) {
+        console.log(det)
+        // document.getElementById("mainCircle").style.transform = `translateX(${det.clientX}px) , translateY(${det.clientY}px)`;
+    })
+}
+mouseFollower()
